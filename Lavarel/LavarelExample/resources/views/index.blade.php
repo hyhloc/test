75fad29b221d8d5369c297b077dc8231042d55ae
hyhloc <hloc75770@gmail.com>
2023-11-30 22:35:49 +0700

    test

---
+++ Lavarel/LavarelExample/resources/views/index.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tài liệu</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="css/tintuc.css">
+    <link rel="stylesheet" href="css/ds.css">
+    
+</head>
+<div>
+    <div class="profile" id="navbar">
+        <div class="profile1">
+            <div class="phone">
+                <div class="phone1">
+                    <i id="mg1" class="fa-solid fa-envelope"></i>
+                    <p>hloc75770@gmail.com</p>
+                </div>
+                <div class="phone2">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>0932341051</p>
+                </div>
+            </div>
+            <div class="link">
+                <i id="lm1" class="fa-brands fa-facebook-f" href=""><a href="https://www.facebook.com/loccmap"></a></i>
+                <i id="lm2" class="fa-brands fa-instagram"></i>
+            </div>
+        </div>
+    </div>
+    <header id="bg-sl">
+        <nav>
+            <div class="nav">
+                <div class="logo">
+                    <img src="image/logooo.png" alt="">
+                </div>
+                <div class="links">
+                    <ul class="links1">
+                        <li id="li1">
+                            <a id="home" href="">Trang chủ</a>
+                        </li>
+                        <li id="li2">
+                            <a href="">Địa điểm</a>
+                        </li>
+                        <li id="li3">
+                            <a href="">Ẩm thực</a>
+                        </li>
+                        <li id="li4">
+                            <a href="">Dịch vụ</a>
+                        </li>
+                        <div>
+                        <li id="li5" class="profiles">
+                            <a href=""><i class="fa-regular fa-user"></i></a>
+                        </li>
+                        </div>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="container-event mt-5">
+        <div class="heading-custom-green">
+            <h2 class="text--green">CÁC TIN TỨC LIÊN QUAN</h2>
+            <div class="line"></div>
+        </div>
+                <hr>
+              </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row content">
+                  <div class="col-sm-4 sidenav">
+                    <ul class="list-group">
+                        <li class="list-group-item"><a href="https://www.mdta.com.vn/tin-tuc/dong-bang-song-cuu-long-du-khach-quoc-te-tang-636-so-voi-cung-ky-nam-2022.html"><img src="image/tt01.jpg" alt="" class="img1"><span class="item-number" style="padding-left: 10px;">Đồng bằng sông Cửu Long: Du khách quốc tế tăng 636% so với cùng kỳ năm 2022<br><i>Hiện nay, khu vực ĐBSCL có 2 Cụm liên kết, hợp tác phát triển du lịch là phía Đông và phía Tây ĐBSCL. <br>Trong đó, Cụm phía Tây (Cần Thơ, An Giang, Kiên Giang
+                             - Vùng kinh tế trọng điểm vùng ĐBSCL và các tỉnh Bạc Liêu, Sóc Trăng, Hậu Giang)</i></span></a></li>
+                        <li class="list-group-item"><a href="https://www.mdta.com.vn/thong-bao/danh-sach-44-diem-du-lich-tieu-bieu-dbscl.html"><img src="image/tt02.jpg" class="img1" alt=""><span class="item-number" style="padding-left: 10px;">Danh sách 44 địa điểm du lịch Đồng Bằng Sông Cửu Long<br><i>Dưới đây là 44 địa điểm du lịch được du khách lựa chọn trong các kỳ nghỉ lễ<br> Những địa điểm du lịch này đều có những đặc trưng riêng và các văn hóa riêng biệt.</i></span></a></li>
+                        <li class="list-group-item"><a href="https://bvhttdl.gov.vn/can-tho-xay-dung-san-pham-du-lich-dac-trung-20231124152819556.htm"><img src="image/tt03.jpg" class="img1" alt=""><span class="item-number" style="padding-left: 10px;">Cần thơ xây dựng sản phẩm du lịch đặc trưng <br><i>Cần Thơ xác định thế mạnh đặc thù là du lịch sông nước và MICE (du lịch kết hợp hội nghị, khen thưởng, sự kiện, triển lãm).Giúp phát triển du lịch ĐBSCL</i></span></a></li>
+                        <li class="list-group-item"><a href=""><img src="image/tt.05.jpg" class="img1" alt=""><span class="item-number" style="padding-left: 10px;">Du lịch Đồng bằng sông Cửu Long: Cơ hội làm mới mình sau đại dịch<br><i>Vốn là một khu vực trù phú, có tài nguyên thiên nhiên đa dạng và được xác định là một trong bảy phân vùng du lịch của cả nước<br> Đồng bằng sông Cửu Long có đầy đủ tiềm năng và đặc trưng văn hóa riêng biệt để phát triển thành một điểm đến nghỉ dưỡng quan trọng ở khu vực phía Nam.</i></span></a></li>
+                    </ul>
+                    </div>
+                  </div>
+              
+                  <div class="container-event mt-5">
+                    <div class="heading-custom-green">
+                        <h2 class="text--green">Một số ẩm thực nổi tiếng của vùng Đồng bằng sông Cửu Long</h2>
+                        <div class="line"></div>
+                    </div>
+                              <hr>
+                          </div>
+                        </div>
+                        <div class="box">
+                            <div class="container" style="padding-left:20px">
+                                <a href="https://mia.vn/cam-nang-du-lich/bun-ca-chau-doc-dac-san-tru-danh-cua-dat-an-giang-8263">
+                                    <img src="image/ds01.png" alt="" class="image">
+                                    <div class="overlay">Bún cá Châu Đốc</div>
+                                </a>
+                            </div>
+                            <div class="container" style="padding-left:20px">
+                                <a href="https://mia.vn/cam-nang-du-lich/lau-mam-an-giang-mon-an-dai-dien-cho-nen-am-thuc-mien-dat-tay-nam-8595">
+                                    <img src="image/ds02.png" alt="" class="image">
+                                    <div class="overlay">Lẩu mắm An Giang</div>
+                                </a>
+                            </div>
+                            <div class="container" style="padding-left: 20px;">
+                                <a href="https://www.traveloka.com/vi-vn/explore/culinary/banh-xeo-dac-san-o-dau/155928">
+                                    <img src="image/ds03.png" alt="" class="image">
+                                    <div class="overlay">Đặc sản bánh xèo miền Tây</div>
+                                </a>
+                            </div>
+                            <div class="container" style="padding-left: 20px;">
+                                <a href="https://nongsandungha.com/thuc-pham/duong-dua">
+                                    <img src="image/ds04.png" alt="" class="image">
+                                    <div class="overlay">Đặc sản đuông dừa</div>
+                                </a>
+                            </div>
+                            
+                            <div class="container" style="padding-left: 20px;">
+                                <a href=https://thamhiemmekong.com/thong-tin-du-lich-mien-tay/khu-thuc-lang-bot-sa-dec-dong-thap.html">
+                                    <img src="image/ds05.jpg" alt="" class="image">
+                                    <div class="overlay">Bánh bột sa đéc</div>
+                                </a>
+                            </div>
+                        </div>
+                        <footer style="background-color:#80aca1">
+                            <p>Copyright © 2022</p>
+                          </footer>
+                        
+           
+</body>
+</html>
